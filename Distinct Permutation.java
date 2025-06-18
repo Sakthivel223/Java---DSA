65d2203e31c3cca069f571bcb2259002df9b0004
Sakthivel S <sankaransakthivel86@gmail.com>
2025-06-18 11:34:50 +0530

    Distinct Permutation.java

--- Distinct Permutation.java
+++ Distinct Permutation.java
@@ -1,49 +1,28 @@
 import java.util.*;
 class Main
-{
-  static boolean f = false;
-  public static void main(String[]args)
   {
-    Scanner s = new Scanner(System.in);
-    int n = s.nextInt();
-    char a[][] = new char[n][n];
-    for(char[] row : a)
-      Arrays.fill(row,'.');
-    HashSet<Integer> cols = new HashSet<>();
-    HashSet<Integer> pd = new HashSet<>();
-    HashSet<Integer> nd = new HashSet<>();
-    backtrack(a,0,n,cols,pd,nd);
-    if(!f) System.out.print("-1");
-  }
-  public static void backtrack(char a[][], int r, int n, HashSet<Integer> cols, HashSet<Integer> pd, HashSet<Integer> nd)
-  {
-    if(f) return;
-    if(r == n)
+    public static void main(String[]args)
     {
-    for(int i=0;i<n;i++)
+      Scanner s = new Scanner(System.in);
+      String str = s.next();
+      HashSet<String> set = new HashSet<>();
+      perm(str,"",set);
+    }
+    public static void perm(String str, String ans, HashSet <String> set)
     {
-      for(int j=0;j<n;j++)
+      if(str.length() == 0)
       {
-        if(a[i][j] == 'Q')
-          System.out.print((j + 1) + " ");
-      }
+        if(!set.contains(ans))
+        {
+          set.add(ans);
+          System.out.println(ans);
+        }
+        return;
     }
-      f = true;
-      return;
-    }
-    for(int c = 0; c < n; c++)
-    {
-      if(cols.contains(c) || pd.contains(r + c) || nd.contains(r - c))
-        continue;
-      a[r][c] = 'Q';
-      cols.add(c);
-      pd.add(r + c);
-      nd.add(r - c);
-      backtrack(a,r + 1, n, cols, pd, nd);
-      a[r][c] = '.';
-      cols.remove(c);
-      pd.remove(r + c);
-      nd.remove(r - c);
-    } 
+      for(int i = 0, i < str.length() ; i++)
+        {
+          char ch = str.charAt(i);
+          String rest = str.substring(0,i) + str.substring(i + 1);
+          perm(res, ans + ch, set);
+        }
   }
-}
